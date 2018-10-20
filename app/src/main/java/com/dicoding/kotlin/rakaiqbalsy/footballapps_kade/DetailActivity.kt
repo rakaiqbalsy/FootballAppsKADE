11262a3bffa4ae1e48ac7e6c3c73b99a3bef2f14
rakaiqbalsy <rakaiqbalsy@gmail.com>
2018-10-21 00:21:27 +0700

    Update1

--- app/src/main/java/com/dicoding/kotlin/rakaiqbalsy/footballapps_kade/DetailActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/rakaiqbalsy/footballapps_kade/DetailActivity.kt
@@ -1,16 +1,17 @@
 package com.dicoding.kotlin.rakaiqbalsy.footballapps_kade
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.padding
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
 
 class DetailActivity : AppCompatActivity() {
 
-    
+    private lateinit var logoClub: ImageView
 
     private var name: String = ""
     private lateinit var nameTextView: TextView
@@ -25,22 +26,48 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         verticalLayout{
             padding = dip(14)
+            gravity = Gravity.CENTER_HORIZONTAL
 
-            nameTextView = textView()
+            logoClub = imageView{
+                setImageResource(R.drawable.img_barca)
+            }. lparams(width = 150, height = 150) {
+                gravity = Gravity.CENTER
+                margin = dip(20)
+            }
+
+            nameTextView = textView {
+                textSize = 24F
+                typeface = Typeface.DEFAULT_BOLD
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }. lparams{
+                width = matchParent
+            }
+
+            deskripsiClub = textView{
+                textSize = 15F
+                typeface = Typeface.DEFAULT_BOLD
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }. lparams {
+                width = matchParent
+                bottomMargin = dip(12)
+            }
 
             stadionName = textView()
 
-            deskripsiClub = textView()
         }
 
         val intent = intent
+
+        //getExtra Intent
+
         name = intent.getStringExtra("name")
-        nameTextView.text = name
-
         stadion = intent.getStringExtra("stadion")
-        stadionName.text = stadion
-
         deskripsi = intent.getStringExtra("deskripsi")
+
+        //setIntent
+        nameTextView.text = name
+        stadionName.text = stadion
         deskripsiClub.text = deskripsi
+        logoClub.setImageResource(intent.getIntExtra("image", R.drawable.img_barca))
     }
 }
--- app/src/main/java/com/dicoding/kotlin/rakaiqbalsy/footballapps_kade/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/rakaiqbalsy/footballapps_kade/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dicoding.kotlin.rakaiqbalsy.footballapps_kade
+ package com.dicoding.kotlin.rakaiqbalsy.footballapps_kade
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
