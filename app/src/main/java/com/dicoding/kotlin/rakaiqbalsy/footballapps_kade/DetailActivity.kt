3d49be0bdf1c3707edb4d8307b5a1c7b8cf514b4
rakaiqbalsy <rakaiqbalsy@gmail.com>
2018-10-23 00:02:57 +0700

    Update2

--- app/src/main/java/com/dicoding/kotlin/rakaiqbalsy/footballapps_kade/DetailActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/rakaiqbalsy/footballapps_kade/DetailActivity.kt
@@ -7,6 +7,7 @@ import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
 
 class DetailActivity : AppCompatActivity() {
